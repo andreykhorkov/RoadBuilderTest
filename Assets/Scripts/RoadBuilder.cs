af040f87845a37fa13df71923eae6fc262d7495c
andreykhorkov <andrey.khorkov@borgward.com>
2018-11-08 19:15:56 -0800

    +

---
+++ Assets/Scripts/RoadBuilder.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RoadBuilder : MonoBehaviour
+{
+    public static Mesh CreateRoadMesh(Vector3[] anchors, float width = 2)
+    {
+        var mesh = new Mesh();
+        var vertices = new Vector3[anchors.Length * 2];
+        var triangles = new int[2 * (vertices.Length - 1)];
+
+
+        return mesh;
+    }
+}
