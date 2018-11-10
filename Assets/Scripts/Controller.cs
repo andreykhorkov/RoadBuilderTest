ea1c368f2fb33f3b10cc43cdd82a2023aa0c34f3
andreykhorkov <andrey.khorkov@borgward.com>
2018-11-09 19:26:54 -0800

    +

--- Assets/Scripts/Controller.cs
+++ Assets/Scripts/Controller.cs
@@ -5,4 +5,39 @@ using UnityEngine;
 public class Controller : MonoBehaviour
 {
     [SerializeField] private RoadBuilder roadBuilder;
+
+    private Vector3[] vectors;
+
+    void Start()
+    {
+        vectors = new Vector3[200];
+
+        for (int i = 0; i < vectors.Length; i++)
+        {
+            vectors[i] = Vector3.forward * i + Vector3.right * 0.1f * i;
+        }
+
+        var go = new GameObject();
+        var meshFilter = go.AddComponent<MeshFilter>();
+        go.AddComponent<MeshRenderer>();
+
+        var mesh = RoadBuilder.CreateRoadMesh(vectors, 5);
+        meshFilter.mesh = mesh;
+
+    }
+
+    void OnDrawGizmos()
+    {
+        if (ReferenceEquals(vectors, null))
+        {
+            return;
+        }
+
+        Gizmos.color = Color.red;
+
+        for (int i = 0; i < vectors.Length; i++)
+        {
+            Gizmos.DrawSphere(vectors[i], 0.1f);
+        }
+    }
 }
--- Assets/Scripts/RoadBuilder.cs
+++ Assets/Scripts/RoadBuilder.cs
@@ -6,11 +6,54 @@ public class RoadBuilder : MonoBehaviour
 {
     public static Mesh CreateRoadMesh(Vector3[] anchors, float width = 2)
     {
-        var mesh = new Mesh();
+        
         var vertices = new Vector3[anchors.Length * 2];
-        var triangles = new int[2 * (vertices.Length - 1)];
+        var triangles = new int[3 * 2 * (anchors.Length - 1)];
+        var uvs = new Vector2[vertices.Length];
+        var vertexIndex = 0;
+        var triIndex = 0;
+        var direction = Vector3.zero;
 
+        for (int anchorIndex = 0; anchorIndex < anchors.Length; anchorIndex++)
+        { 
+            Vector3 right;
 
+            if (anchorIndex < anchors.Length - 1)
+            {
+                direction = (anchors[anchorIndex + 1] - anchors[anchorIndex]).normalized;
+            }
+
+            right = Vector3.Cross(direction, Vector3.up);
+
+            vertices[vertexIndex] = anchors[anchorIndex] + right * 0.5f * width;
+            vertices[vertexIndex + 1] = anchors[anchorIndex] - right * 0.5f * width;
+
+            var completionPercent = anchorIndex / (float) (anchors.Length - 1);
+            var v = 1 - Mathf.Abs(2 * completionPercent - 1);
+            uvs[vertexIndex] = new Vector2(0, v);
+            uvs[vertexIndex + 1] = new Vector2(1, v);
+
+            if (anchorIndex < anchors.Length - 1)
+            {
+                triangles[triIndex] = vertexIndex;
+                triangles[triIndex + 1] = vertexIndex + 2;
+                triangles[triIndex + 2] = vertexIndex + 1;
+                triangles[triIndex + 3] = vertexIndex + 1;
+                triangles[triIndex + 4] = vertexIndex + 2;
+                triangles[triIndex + 5] = vertexIndex + 3;
+            }
+
+            vertexIndex += 2;
+            triIndex += 6;
+        }
+
+        var mesh = new Mesh
+        {
+            vertices = vertices,
+            triangles = triangles,
+            uv = uvs
+        };
+        
         return mesh;
     }
 }
